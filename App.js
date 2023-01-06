30788552879ad0b2726435eb0812bca3737f25ff
Syed0208 <36785858+Syed0208@users.noreply.github.com>
2023-01-07 00:25:36 +0530

    Intro to React course

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1", {}, "Heyyy!!");
+const heading2 = React.createElement("h2", {}, "React!!");
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
